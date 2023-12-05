f25ec7faea83051848947755a30a625c45e77a9c
Travis <travisdanielbrown@proton.me>
2023-12-05 16:24:18 -0500

    created discs slice of state - actions, creators, reducer

---
+++ react-app/src/store/discs.js
@@ -0,0 +1,197 @@
+//Actions
+const get_discs = 'discs/get_Discs'
+const get_one_disc = 'discs/get_One_Disc'
+const get_user_discs = 'discs/get_user_discs'
+const create_disc = 'discs/create_Disc'
+const update_disc = 'discs/update_Disc'
+const delete_disc = 'discs/delete_Disc'
+
+
+//Action Creators
+const getDiscs = (discs) => {
+    return {
+        type: get_discs,
+        discs
+    }
+}
+
+const getOneDisc = (disc) => {
+    return {
+        type: get_one_disc,
+        disc
+    }
+}
+
+const getUserDiscs = (discs) => {
+    return {
+        type: get_user_discs,
+        discs
+    }
+}
+
+const createDisc = (disc) => {
+    return {
+        type: create_disc,
+        disc
+    }
+}
+
+const updateDisc = (disc) => {
+    return {
+        type: update_disc,
+        disc
+    }
+}
+
+const deleteDisc = (discId) => {
+    return {
+        type: delete_disc,
+        discId
+    }
+}
+
+
+//Thunks
+
+//load all discs
+export const getAllDiscs = () => async dispatch => {
+    const res = await fetch('/api/discs')
+
+    if (res.ok) {
+        const discs = await res.json()
+        dispatch(getDiscs(discs))
+        return discs
+    }else {
+        const errors = await res.json()
+        return errors
+    }
+}
+
+//Load one disc
+export const getDiscDetails = (id) => async dispatch => {
+    const res = await fetch(`/api/discs/${id}`)
+
+    if(res.ok) {
+        const disc = await res.json()
+        dispatch(getOneDisc(disc))
+        return disc
+    }else {
+        const errors = await res.json()
+        return errors
+    }
+}
+
+//Load all user discs
+export const getAllUserDiscs = () => async dispatch => {
+    const res = await fetch('/api/discs/current')
+
+    if(res.ok) {
+        const discs = await res.json()
+        dispatch(getUserDiscs(discs))
+        return discs
+    }else {
+        const errors = await res.json()
+        return errors
+    }
+}
+
+//Create a new disc
+export const createADisc = (disc) => async dispatch => {
+    const res = await fetch('/api/discs/new', {
+        method: 'POST',
+        body: disc
+    })
+
+    if(res.ok) {
+        const newDisc = await res.json()
+        dispatch(createDisc(newDisc))
+        return newDisc
+    }else {
+        const errors = await res.json()
+        return errors
+    }
+
+}
+
+//Update an existing disc
+export const updateADisc = (disc, discId) => async dispatch => {
+    const res = await fetch(`/api/discs/update/${discId}`, {
+        method: 'PUT',
+        body: disc
+    })
+
+    if(res.ok) {
+        const updatedDisc = await res.json()
+        dispatch(updateDisc(updatedDisc))
+        return updatedDisc
+    }else {
+        const errors = await res.json()
+        return errors
+    }
+}
+
+//Delete a disc
+export const deleteADisc = (discId) => async dispatch => {
+    const res = await fetch(`/api/discs/${discId}`, {
+        method: 'DELETE'
+    })
+
+    if(res.ok) {
+        dispatch(deleteDisc(discId))
+    }else {
+        const errors = await res.json()
+        return errors
+    }
+}
+
+
+//Discs Reducer
+const initState = {allDiscs: {}, userDiscs: {}}
+
+export default function discsReducer(state = initState, action) {
+    let newState;
+    switch (action.type) {
+        case get_discs:
+            newState = {...state, allDiscs: {}}
+            action.discs.forEach(disc => newState.allDiscs[disc.id] = disc)
+            return newState
+
+        case get_one_disc:
+            newState = {...state, allDiscs: {...state.allDiscs}}
+            newState.allDiscs[action.disc.id] = action.disc
+            return newState
+
+        case create_disc:
+            newState = {...state, allDiscs: {...state.allDiscs}}
+            newState.allDiscs[action.disc.id] = action.disc
+            return newState
+
+        case update_disc:
+            newState = {...state, allDiscs: {...state.allDiscs}}
+            newState.allDiscs[action.disc.id] = action.disc
+            return newState
+
+        case get_user_discs:
+            newState = {...state, userDiscs: {}}
+            action.discs.foeEach(disc => newState.userDiscs[disc.id] = disc)
+            return newState
+
+        case delete_disc:
+             newState = {...state, allDiscs: {...state.allDiscs}}
+             delete newState.allDiscs[action.discId]
+             return newState
+
+    }
+}
+
+
+
+
+
+
+// const get_Discs = 'discs/get_Discs'
+// const get_One_Disc = 'discs/get_One_Disc'
+// const get_user_discs = 'discs/get_user_discs'
+// const create_Disc = 'discs/create_Disc'
+// const update_Disc = 'discs/update_Disc'
+// const delete_Disc = 'discs/delete_Disc'
