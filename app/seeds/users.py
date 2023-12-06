0f500e3639f6edf316ca6981cb3f9cd9fceb365b
Travis <travisdanielbrown@proton.me>
2023-12-06 16:41:56 -0500

    needed to change some of the seed data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -7,11 +7,11 @@ def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='Marnie', email='marnie@aa.io', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='Bobbie', email='bobbie@aa.io', password='password')
     tommy = User(
-        username='tommy', email='tommy@aa.io', password='password')
+        username='Tommy', email='tommy@aa.io', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
