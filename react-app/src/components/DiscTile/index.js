416c4d2bb1f977f0386971c1b29e136378b57db7
Travis <travisdanielbrown@proton.me>
2023-12-11 19:34:10 -0500

    added update and delete btns to ui

--- react-app/src/components/DiscTile/index.js
+++ react-app/src/components/DiscTile/index.js
@@ -6,7 +6,8 @@ import './DiscTile.css'
 const DiscTile = ({disc}) => {
     const location = useLocation()
     return (
-        <Link to={`/${disc.id}`} className='disc-details-link'>
+        <div>
+            <Link to={`/${disc.id}`} className='disc-details-link'>
             <div className="disc-tile">
                 <div className="disc-img-container" >
                     <img src={disc.photoUrl} alt="" className="disc-img" />
@@ -19,16 +20,17 @@ const DiscTile = ({disc}) => {
                         <div>Glide<p>{disc.glide}</p></div>
                         <div>Turn<p>{disc.turn}</p></div>
                         <div>Fade<p>{disc.fade}</p></div>
+                    </div>
                 </div>
+            </div>
+            </Link>
                 {location.pathname === '/current' &&
                     <div className="manage-btns">
                         <button className="update">Update</button>
                         <button className="delete">Delete</button>
                     </div>
                 }
-                </div>
-            </div>
-        </Link>
+        </div>
     )
 }
 export default DiscTile
